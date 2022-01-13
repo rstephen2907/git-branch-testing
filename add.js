f581aac27d055d2a52afbdcbddfc33f4a769a1f2
rstephen2907 <rstephen2000@gmail.com>
2022-01-13 14:24:52 +0530

    add function for n numbers

--- add.js
+++ add.js
@@ -1,4 +1,13 @@
 // addition function to add 2 numbers
 const add = (a, b) => a + b;
 
+// addition function to add n numbers
+const addn = (...x) => {
+    let sum = 0;
+    x.forEach((ele) => {
+        sum += ele;
+    });
+    return sum;
+}
+
 module.exports = add;
