7bbfa827549d95cea9b4707834a157683c901b0e
rstephen2907 <rstephen2000@gmail.com>
2022-01-13 18:20:57 +0530

    subtration for n numbers

--- sub.js
+++ sub.js
@@ -1,4 +1,9 @@
 // function to subtract 2 numbers
-const sub = (a, b) => a - b;
+const sub1 = (a, b) => a - b;
 
-module.exports = sub;
+const subn = (...x) => {
+    const sum = x.reduce((prev, curr) => prev + curr);
+    return sum;
+} 
+
+console.log(subn(1, 2, 3, 4));
