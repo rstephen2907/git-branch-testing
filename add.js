b2ebc1cfe12c71f4416838b986e96f585e857aa8
rstephen2907 <rstephen2000@gmail.com>
2022-01-13 18:06:58 +0530

    review changes applied

--- add.js
+++ add.js
@@ -1,6 +1,9 @@
 // addition function to add 2 numbers
 const add = (a, b) => a + b;
 
+// addtion function to add 3 numbers
+const add3 = (a, b, c) => a + b + c;
+
 // addition function to add n numbers
 const addn = (...x) => {
     let sum = 0;
@@ -9,7 +12,5 @@ const addn = (...x) => {
     });
     return sum;
 }
-// addtion function to add 3 numbers
-const add3 = (a, b, c) => a + b + c;
 
 module.exports = add;
